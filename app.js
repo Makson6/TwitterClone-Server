114d115dcbeceb778b57b519c2f4fb882429a8b5
CM <143717996+Makson6@users.noreply.github.com>
2024-05-11 23:25:09 +0300

    suppresion du fichier.env

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
 import express from "express";
-import dotenv from "dotenv";
 import cors from "cors";
 import currentUserRoute from "./routes/currentuser.routes.js";
 import tweetsRoute from "./routes/tweets.routes.js";
 import usersRoute from "./routes/users.routes.js";
 
-
-dotenv.config();
 const app = express();
-const {PORT} = process.env;
-
+const PORT = 8000;
 app.use(cors())
 app.use(express.json());
 app.use("/tweets", tweetsRoute)
 app.use("/users", usersRoute)
 app.use("/currentUser", currentUserRoute)
-
-
 
 app.listen(PORT, () => 
     console.log(`ce serveur http://localhost:${PORT}`))
